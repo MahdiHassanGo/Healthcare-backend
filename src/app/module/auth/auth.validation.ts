8df71d3f84bf3289202588b28ef75a6e59183b03
MahdiHassanGo <contact.mahdiasif@gmail.com>
2026-08-18 14:34:17 +0600

    update with redis forgot password

--- src/app/module/auth/auth.validation.ts
+++ src/app/module/auth/auth.validation.ts
@@ -14,6 +14,12 @@ import z from "zod";
         contactNumber: z.string().optional()
     }).optional()
 })
+ const PatientEmailVerifyZodSchema = z.object({
+    
+    email: z.email("Not email!!"),
+     otp: z.string().length(6)
+   
+})
 
 const LoginZodSchema = z.object({
     email : z.email(),
@@ -44,6 +50,7 @@ const ResetPasswordZodSchema = z.object({
 
 export const UserValidation = {
     PatientRegistrationZodSchema,
+    PatientEmailVerifyZodSchema,
     LoginZodSchema,
     ForgotPasswordZodSchema,
     ResetPasswordZodSchema
